9677cee393369168b38ce5240aa16a29238bf4a4
Hamza Cherkaoui <hamzacherkaoui806@gmail.com>
2024-06-23 18:11:08 +0100

    I Finish on navbar

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+// ! Components
+import Header from "@/components/Header";
+
+const jetBrainsMono= JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300","400", "500", "600", "700", "800"],
+  variable: "--font-jetBrainsMono",
+});
+
+export const metadata = {
+  title: "Hamza Portfolio",
+  description: "Hamza Cherkaoui Portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={jetBrainsMono.variable}>
+        <Header />
+        {children}
+        </body>
+    </html>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import Nav from './Nav'
+import { Button } from './ui/button'
+
+function Header() {
+  return (
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+      <div className="container mx-auto flex justify-between items center">
+        {/* // ! Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Hamza<spran className="text-accent">.</spran>
+          </h1>
+        </Link>
+
+        {/* // ! Desktop Navbar */}
+        <div className="hidden xl:flex items-center gap-7">
+          <Nav />
+          <Link href="/content" className="">
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+
+
+        {/* //! mobile nav */}
+          <div className="xl:hidden">mobile nav</div>
+
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
