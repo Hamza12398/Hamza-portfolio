e5472194cf854cfdf0a16b24e52dce5810bec745
Hamza Cherkaoui <hamzacherkaoui806@gmail.com>
2024-07-02 16:56:16 +0100

    Finish In NavBar  and working With Home PAge

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,29 @@
-export default function Home() {
+import {FiDownload} from "react-icons/fi"
+
+
+const Home = () => {
   return (
-    <h1>Home Page</h1>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col items-center justify-between xl:flex-row xl:pt-8 xl:pb-22">
+          {/* Text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Front-End Developer</span>
+            <h1 className="h1">
+              Hello I'm <span className="text-accent">Hamza</span>
+            </h1>
+            <p className="max-w-[480px] mb-9 text-white/70">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolorem quaerat, consequatur reiciendis aliquam saepe consectetur, eveniet illo rerum dolor, quod suscipit dolore beatae quam ea consequuntur ut ducimus recusandae?</p>
+          </div>
+            
+            {/*Buttons and Social Media  */}
+              
+
+          {/* Image */}
+          <div>cherkaoui</div>
+          </div>
+      </div>
+    </section>
   );
 }
+
+export default Home
--- components/MobilNav.jsx
+++ components/MobilNav.jsx
@@ -1,16 +1,69 @@
 "use client";
 
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
 
-import React from 'react'
-import {Sheet, SheetContent, SheetTrigger} from "./ui/sheet"
-import {usePathname} from "next/navigation"
-
+const links = [
+	{
+		name: "Home",
+		path: "/",
+	},
+	{
+		name: "services",
+		path: "/services",
+	},
+	{
+		name: "resume",
+		path: "/resume",
+	},
+	{
+		name: "work",
+		path: "/work",
+	},
+	{
+		name: "contact",
+		path: "/contact",
+	},
+];
 const MobilNav = () => {
-  return (
-    <nav>
-      <Sheet />
-    </nav>
-  )
-}
+	const path = usePathname();
+	return (
+		<Sheet>
+			<SheetTrigger className="p-3 flex justify-center items-center">
+				<CiMenuFries className="text-[30px] text-accent" />
+			</SheetTrigger>
+			<SheetContent className="flex flex-col bg-slate-600">
+				{/* LOGO */}
+				<div className="mt-32 text-center text-2xl mb-40">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Hamza<spran className="text-accent">.</spran>
+            </h1>
+          </Link>
+        </div>
+
+				{/* NAV */}
+				<nav className="flex flex-col gap-6 justify-center items-center">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === path && "text-accent border-b-2 border-accent"
+                } capitalize font-medium hover:text-accent transition-all}`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+			</SheetContent>
+		</Sheet>
+	);
+};
 
 export default MobilNav;
